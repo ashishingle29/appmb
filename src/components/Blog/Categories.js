088d262c2d4aa832f66eefc0ae9f7ff2decd4f47
Ashish Ingle WeDev <92534028+ashishingle29@users.noreply.github.com>
2025-04-26 23:13:01 +0530

    done

--- src/components/Blog/Categories.js
+++ src/components/Blog/Categories.js
@@ -4,9 +4,9 @@ import Category from "./Category";
 const Categories = ({ categories, currentSlug , module}) => {
   return (
     <div className=" px-0 md:px-10 sxl:px-20 mt-10 border-t-2 text-dark dark:text-light border-b-2 border-solid border-dark dark:border-light py-4 flex items-start flex-wrap font-medium mx-5 md:mx-10">
-      {categories.map((cat) => (
+      {categories.map((cat, index) => (
         <Category
-          key={cat._id}
+          key={index}
           link={`/${module}/${cat.slug}`}
           name={cat.name}
           active={cat?.slug === currentSlug}
